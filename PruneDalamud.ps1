bc77d24ed99d2098df96f0cac58a380c1ff0c578
Bluefissure <bluefissureofg@gmail.com>
2023-11-10 22:10:04 -0600

    Update PruneDalamud.ps1

--- PruneDalamud.ps1
+++ PruneDalamud.ps1
@@ -5,4 +5,13 @@ Remove-Item -Force -Recurse .\scratch\runtimes\osx-x64 -ErrorAction SilentlyCont
 Remove-Item -Force -Recurse .\scratch\runtimes\unix -ErrorAction SilentlyContinue
 Remove-Item -Force -Recurse .\scratch\runtimes\win -ErrorAction SilentlyContinue
 Remove-Item -Force -Recurse .\scratch\runtimes\win-x86 -ErrorAction SilentlyContinue
+Remove-Item -Force -Recurse .\scratch\runtimes\alpine-x64 -ErrorAction SilentlyContinue
+Remove-Item -Force -Recurse .\scratch\runtimes\linux-arm -ErrorAction SilentlyContinue
+Remove-Item -Force -Recurse .\scratch\runtimes\linux-arm64 -ErrorAction SilentlyContinue
+Remove-Item -Force -Recurse .\scratch\runtimes\linux-armel -ErrorAction SilentlyContinue
+Remove-Item -Force -Recurse .\scratch\runtimes\linux-mips64 -ErrorAction SilentlyContinue
+Remove-Item -Force -Recurse .\scratch\runtimes\linux-musl-x64 -ErrorAction SilentlyContinue
+Remove-Item -Force -Recurse .\scratch\runtimes\linux-x86 -ErrorAction SilentlyContinue
+Remove-Item -Force -Recurse .\scratch\runtimes\win-arm -ErrorAction SilentlyContinue
+Remove-Item -Force -Recurse .\scratch\runtimes\win-arm64 -ErrorAction SilentlyContinue
 Remove-Item -Force -Recurse .\scratch\hashes.json -ErrorAction SilentlyContinue
